834521fd70b51cbc65bd38b8357a37b19014cb18
Matheus Luis <66440299+causticroot@users.noreply.github.com>
2021-04-03 13:24:59 -0300

    Adiciona foreach explained
    ler:
    https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/inside-a-program/coding-conventions
    https://docs.microsoft.com/pt-br/dotnet/csharp/language-reference/keywords/foreach-in
    https://docs.microsoft.com/pt-br/dotnet/api/system.collections.generic.list-1?view=net-5.0

--- Lista 2/Program.cs
+++ Lista 2/Program.cs
@@ -10,31 +10,30 @@ namespace Lista_2
     {
         static void Main(string[] args)
         {
-            List<Funcionario> List = new List<Funcionario>();
+            //nao utilize letras maiusculas nessas variaveis, leia as conveções de código do csharp
+            // https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/inside-a-program/coding-conventions
+            List<Funcionario> list = new List<Funcionario>();
 
             Funcionario f1 = new Funcionario(333,"Maria Brown", 4000);
             Funcionario f2 = new Funcionario(536,"Alex Grey", 3000);
             Funcionario f3 = new Funcionario(333,"Bob Green", 5000);
 
-            List.Add(f1);
-            List.Add(f2);
-            List.Add(f3);
+            list.Add(f1);
+            list.Add(f2);
+            list.Add(f3);
 
 
             Console.WriteLine("Digite o ID do funcionario");
             int searchId = int.Parse(Console.ReadLine());
 
             
-            
-            foreach (Funcionario x in List)
-            {  
-               
-                Console.WriteLine();
+            //cada objeto da List "lista" é armazenado em item (uma var do tipo Funcionario) em cada loop, em sequencia de fila
+            foreach (Funcionario item in list)
+            {
+                // se o id que o usuario entrou for igual o id do objeto do respectivo loop, imprima
+                if (searchId == item.Id)
+                    Console.WriteLine($"id - {item.Id} / nome - {item.Nome} / salario - {item.Salario}");
             }
-
-
-
-
         }
     }
 }
